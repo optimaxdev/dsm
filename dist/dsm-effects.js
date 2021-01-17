91d23c39cdc4c59b08d7268b6b0925fa0b13b4ec
Theo Freud <theo.f@glassesusa.com>
2021-01-17 11:45:56 +0200

    Elements should work dynamically now

--- dist/dsm-effects.js
+++ dist/dsm-effects.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.dsmForm .inputLabel input[type=text]').forEach(e => e.addEventListener("change", (
         e) => {
         // Text input styling including effects on change values
@@ -12,49 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }))
 
-    document.querySelectorAll('.dsmButton').forEach(e => e.addEventListener("click", (e) => {
-        let el = e.target.closest("button");
-        if (el.classList.contains('round')) {
-            el.classList.add("disabled");
-
-            el.innerHTML = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform 
-                        attributeName="transform" 
-                        attributeType="XML" 
-                        type="rotate"
-                        dur="1s" 
-                        from="0 50 50"
-                        to="360 50 50" 
-                        repeatCount="indefinite" />
-                </path>
-                </svg>`
-        } else {
-            el.classList.add("disabled");
-            el.innerHTML = (`<div class="progress"><div class="indeterminate"></div></div>` + el.innerHTML);
-            el.setAttribute("disabled", true);
-        }
-    }))
-
-    document.querySelectorAll('.selectContainer button').forEach(e => {
-        e.innerHTML = e.innerHTML + `<svg xmlns="http://www.w3.org/2000/svg" stroke="rgb(137, 149, 156)" width="12px" height="10px">
-                    <path d="M1 1l5 6 5-6" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round"
-                        stroke-linejoin="round"></path>
-                </svg>`;
-        e.addEventListener("click", (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            let el = e.target.closest('.selectContainer');
-            el.querySelector('button').classList.remove("active");
-            if (el.querySelector('ul').getAttribute("style") != null & el.querySelector('ul').style.display == "block") return el.querySelector('ul').style.display = "none"
-            el.querySelector('ul').style.display = "block";
-            el.querySelector('button').classList.add("active");
-        })
-    })
+    document.body.addEventListener("click", function (e) {
+        clickHandler(e);
+    });
 
 
-    document.body.addEventListener("click", (e) => {
+    function clickHandler(e) {
         e.stopPropagation();
         document.querySelectorAll('.selectContainer ul').forEach(e => {
             if (e.getAttribute("style") == null) return;
@@ -81,7 +43,72 @@ document.addEventListener("DOMContentLoaded", function () {
                 el.style.opacity = opacity;
             }, 20);
         })
+
+        let el = e.target;
+        if (el.closest('.dsmButton')) buttonClick(e);
+
+    }
+
+    function closeTooltip(el) {
+        el = el.closest('.dsmTooltip.click').querySelector('.container');
+        let opacity = 1;
+        el.style.opacity = opacity;
+        exitInterval = setInterval(() => {
+            if (opacity < 0.1) {
+                clearInterval(exitInterval)
+                return el.style.visibility = "hidden";
+            };
+            opacity = opacity - 0.1
+            el.style.opacity = opacity;
+        }, 20);
+    }
+
+    function selectItem(value) {
+        null;
+    }
+
+    function buttonClick(e) {
+        e.preventDefault();
+        let el = e.target.closest("button");
+        if (el.classList.contains('round')) {
+            el.classList.add("disabled");
+
+            el.innerHTML = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                    <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                    <animateTransform 
+                        attributeName="transform" 
+                        attributeType="XML" 
+                        type="rotate"
+                        dur="1s" 
+                        from="0 50 50"
+                        to="360 50 50" 
+                        repeatCount="indefinite" />
+                </path>
+                </svg>`
+        } else {
+            el.classList.add("disabled");
+            el.innerHTML = (`<div class="progress"><div class="indeterminate"></div></div>` + el.innerHTML);
+            el.setAttribute("disabled", true);
+        }
+    }
+
+    document.querySelectorAll('.selectContainer button').forEach(e => {
+        e.innerHTML = e.innerHTML + `<svg xmlns="http://www.w3.org/2000/svg" stroke="rgb(137, 149, 156)" width="12px" height="10px">
+                    <path d="M1 1l5 6 5-6" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round"
+                        stroke-linejoin="round"></path>
+                </svg>`;
+        e.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            let el = e.target.closest('.selectContainer');
+            el.querySelector('button').classList.remove("active");
+            if (el.querySelector('ul').getAttribute("style") != null & el.querySelector('ul').style.display == "block") return el.querySelector('ul').style.display = "none"
+            el.querySelector('ul').style.display = "block";
+            el.querySelector('button').classList.add("active");
+        })
     })
+
     document.querySelectorAll('.selectContainer li').forEach(e => e.addEventListener("click", (e) => {
         document.querySelectorAll('.selectContainer li').forEach(e => e.classList.remove('selected'));
         e.target.classList.add("selected");
@@ -192,23 +219,3 @@ document.addEventListener("DOMContentLoaded", function () {
             })
         }
     })
-
-})
-
-function closeTooltip(el) {
-    el = el.closest('.dsmTooltip.click').querySelector('.container');
-    let opacity = 1;
-    el.style.opacity = opacity;
-    exitInterval = setInterval(() => {
-        if (opacity < 0.1) {
-            clearInterval(exitInterval)
-            return el.style.visibility = "hidden";
-        };
-        opacity = opacity - 0.1
-        el.style.opacity = opacity;
-    }, 20);
-}
-
-function selectItem(value) {
-    null;
-}
