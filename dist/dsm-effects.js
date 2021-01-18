7d29dce0cc9956f3bd133ea38c63b10a54cade0c
Theo Freud <theo.f@glassesusa.com>
2021-01-18 15:32:41 +0200

    Who forgets PX?

--- dist/dsm-effects.js
+++ dist/dsm-effects.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let el = e.target.closest("button");
         if (el.classList.contains('round')) {
             el.classList.add("disabled");
-            el.style.width = el.clientWidth;
+            el.style.width = el.clientWidth + 'px';
 
             el.innerHTML = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                 viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
