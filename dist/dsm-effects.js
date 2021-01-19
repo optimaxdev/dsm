104097e687cb82d7e585d69c11ab786214f1783b
Theo Freud <theo.f@glassesusa.com>
2021-01-19 13:33:12 +0200

    More fixes!

--- dist/dsm-effects.js
+++ dist/dsm-effects.js
@@ -43,45 +43,50 @@ document.addEventListener("DOMContentLoaded", function () {
         if (e.querySelector('label').innerText == "") e.querySelector('label').innerText = "Please insert a placeholder";
     })
 
-    document.querySelectorAll('.dsmTooltip').forEach(e => {
-        e.innerHTML = `<div class="item">?</div><div class="container">
+    function adjustTooltip() {
+        document.querySelectorAll('.dsmTooltip').forEach(e => {
+            let text = e.innerText;
+            e.innerHTML = `<div class="item">?</div><div class="container">
             <div class="arrow"></div>
             <div class="infoBox">
-                <div class="textArea">${e.innerText}</div>
+                <div class="textArea">${text}</div>
             </div>
         </div>`
-        const el = e.querySelector('.container');
-        const containerHeight = el.clientHeight;
-        const containerWidth = el.clientWidth;
+            const el = e.querySelector('.container');
+            const containerHeight = el.clientHeight;
+            const containerWidth = el.clientWidth;
 
-        switch (true) {
-            case e.classList.contains('right'):
-                el.style.top = `-${containerHeight /2 - 5 }px`;
-                el.style.left = `-${containerWidth}px`;
-                el.querySelector('.arrow').style.top = `${containerHeight /2 - 4}px`
-                break;
-            case e.classList.contains('left'):
-                el.style.top = `-${containerHeight /2 - 5}px`;
-                el.style.right = `-${containerWidth}px`;
-                el.querySelector('.arrow').style.top = `${containerHeight /2 - 4}px`
-                el.style.left = `30px`;
-                break;
-            case e.classList.contains('bottom'):
-                el.style.top = `30px`;
-                el.style.left = `-${containerWidth /2}px`;
-                break;
-            default:
-                el.style.left = `-${containerWidth /2 - 5}px`;
-                el.style.top = `unset`;
-                el.style.bottom = `30px`;
-                break;
-        }
+            switch (true) {
+                case e.classList.contains('right'):
+                    el.style.top = `-${containerHeight /2 - 5 }px`;
+                    el.style.left = `-${containerWidth}px`;
+                    el.querySelector('.arrow').style.top = `${containerHeight /2 - 4}px`
+                    break;
+                case e.classList.contains('left'):
+                    el.style.top = `-${containerHeight /2 - 5}px`;
+                    el.style.right = `-${containerWidth}px`;
+                    el.querySelector('.arrow').style.top = `${containerHeight /2 - 4}px`
+                    el.style.left = `40px`;
+                    break;
+                case e.classList.contains('bottom'):
+                    el.style.top = `30px`;
+                    el.style.left = `-${containerWidth /2 - 10}px`;
+                    break;
+                default:
+                    el.style.left = `-${containerWidth /2 - 15}px`;
+                    el.style.top = `unset`;
+                    el.style.bottom = `30px`;
+                    break;
+            }
 
-        if (e.classList.contains('close')) {
-            el.innerHTML = el.innerHTML + `<svg onclick="closeTooltip(this)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="100%" height="100%"><g fill="none" stroke="#b0bdc5"><path d="M12.3.7L.6 12.3M.7.7l11.7 11.6"></path></g></svg>`
-        }
-    })
+            if (e.classList.contains('close')) {
+                el.innerHTML = el.innerHTML + `<svg onclick="closeTooltip(this)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="100%" height="100%"><g fill="none" stroke="#b0bdc5"><path d="M12.3.7L.6 12.3M.7.7l11.7 11.6"></path></g></svg>`
+            }
+        })
+    }
+    adjustTooltip();
 })
+
 
 
 function openTooltip(e) {
