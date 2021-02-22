aba2e58b1e595c71e4752e2276f4a1ed8a7ada25
Theo Freud <theo.f@glassesusa.com>
2021-02-22 17:20:28 +0200

    Stop animation running before closing finished

--- js/accordian.js
+++ js/accordian.js
@@ -11,30 +11,36 @@ document.querySelectorAll('.dsmAccordian').forEach((a) => {
     let splitElements = a.innerHTML.split('</summary>')
     a.innerHTML = `${splitElements[0]}</summary><span class="accordianContent">${splitElements[1]}</span>`
   }
+
+  if (a.getAttribute('open') != null) {
+    a.style.minHeight = a.offsetHeight + 'px'
+  }
   a.addEventListener('click', (e) => {
     e.preventDefault()
     let el = e.target.closest('details')
     el.setAttribute('aria-listener', true)
     if (el.getAttribute('open') == null) {
       let count = 0
-      document.querySelectorAll('.dsmAccordian').forEach((a) => {
+      let openElements = document.querySelectorAll('.dsmAccordian[open]')
+      openElements.forEach((a) => {
         let currEl = a
-        count++
-        if (currEl.getAttribute('open') == null) return
         currEl.classList.add('closeAccordian')
         setTimeout(() => {
           currEl.removeAttribute('open')
           currEl.classList.remove('closeAccordian')
+          count++
         }, 200)
         setTimeout(() => {
           currEl.style.minHeight = '0px'
         }, 200)
       })
-
-      if (count == document.querySelectorAll('.dsmAccordian').length) {
-        el.style.minHeight = el.offsetHeight + 'px'
-        el.setAttribute('open', '')
-      }
+      let loopFinished = setInterval(() => {
+        if (count == openElements.length) {
+          clearInterval(loopFinished)
+          el.style.minHeight = el.offsetHeight + 'px'
+          el.setAttribute('open', '')
+        }
+      }, 50)
     } else {
       el.classList.add('closeAccordian')
       setTimeout(() => {
