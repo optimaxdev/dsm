8985af0d361db948ed809ee887c15a8d0475c29b
sahar <sahar.fefer@gmail.com>
2021-09-05 16:03:40 +0300

    update expressAPI/index.js

--- expressAPI/index.js
+++ expressAPI/index.js
@@ -1,7 +1,15 @@
-var express = require('express')
-var app = express()
-const axios = require('axios').default
-require('dotenv').config()
+import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
+
+import * as express from 'express';
+const app = express();
+
+import * as axios from "axios";
+const axios = axios.default
+
+import * as dotenv from "dotenv";
+dotenv.config()
 
 app.get('/', function (req, res) {
   return res.status(403).send('Please use a valid request url')
@@ -60,9 +68,9 @@ app.get('/reviews', function (req, res) {
   })
 })
 
-var http = require('http'),
-  https = require('https')
-var fs = require('fs')
+// var http = require('http'),
+//   https = require('https')
+// var fs = require('fs')
 
 http.createServer(app).listen(1818)
 https
