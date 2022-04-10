a66203512cae02793390e002ed7d90c937b67cf4
Theo Freud <theo.f@glassesusa.com>
2022-04-10 13:39:41 +0300

    Change local api to http

--- src/trustpilot.ts
+++ src/trustpilot.ts
@@ -2,14 +2,15 @@ import { replaceIcons } from './icons'
 import Swiper, { Navigation } from 'swiper'
 Swiper.use([Navigation])
 
-// let apiURL = 'https://api.freud-online.co.uk:3100'
-let apiURL = '/bff/trustpilot'
+let apiURL = 'http://api.freud-online.co.uk:3100'
+// let apiURL = 'https://www.glassesusa.com/bff/trustpilot'
 if (
-  !window.location.href.includes('glassesusa.com') &&
+  !window.location.href.includes('glassesusa.com') ||
   !window.location.href.includes('gusa')
 ) {
-  apiURL = 'https://api.freud-online.co.uk:3100'
+  apiURL = 'https://master-gusa-desktop.gusadev.com/bff/trustpilot'
 }
+
 export function startTrustpilot() {
   let container = document.querySelector('.dsmTrustpilot') as HTMLElement
   if (container.dataset.noScript === 'true') return
