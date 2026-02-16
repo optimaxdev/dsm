4a82fd81077770c5bf8bcef2ef646498e3853280
Sahar Feferovich <sahar.fefer@gmail.com>
2026-02-16 16:07:22 +0200

    Refactor button loading state and add SCSS variables
    - Updated buttonClick function in buttons.ts to enhance loading state with a new SVG animation.
    - Removed old loading logic and replaced it with a more streamlined approach.
    - Added a new _variables.scss file to define color palette and gradients for consistent styling across components.
    - Updated styles.scss to import the new variables file for better organization and maintainability.

--- src/buttons.ts
+++ src/buttons.ts
@@ -3,27 +3,13 @@ export default function buttonClick(e) {
   if (!el) return;
   e.preventDefault();
   e.stopPropagation();
-  if (el.classList.contains("round")) {
-    el.style.width = el.getClientRects()[0].width + "px";
-    el.classList.add("disabled");
-    el.innerHTML = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform 
-                        attributeName="transform" 
-                        attributeType="XML" 
-                        type="rotate"
-                        dur="1s" 
-                        from="0 50 50"
-                        to="360 50 50" 
-                        repeatCount="indefinite" />
-                </path>
-                </svg>${el.innerHTML}`;
-  } else {
-    el.classList.add("disabled");
-    el.innerHTML =
-      `<div class="progress"><div class="indeterminate"></div></div>` +
-      el.innerHTML;
-    el.setAttribute("disabled", true);
-  }
+  el.style.width = el.getClientRects()[0].width + "px";
+  el.classList.add("loading");
+  el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 35">
+  <circle cx="17.5" cy="17.5" r="16" stroke="#fff" stroke-opacity=".5" stroke-width="3" opacity=".3"/>
+  <path stroke="#fff" stroke-linecap="round" stroke-width="3" d="M33.5 17.5a16 16 0 0 1-16 16">
+    <animateTransform attributeName="transform" dur="1s" from="0 17.5 17.5" repeatCount="indefinite" to="360 17.5 17.5" type="rotate"/>
+  </path>
+</svg>
+${el.innerHTML}`;
 }
